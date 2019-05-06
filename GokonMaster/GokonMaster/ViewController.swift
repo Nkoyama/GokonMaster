9b67f8ad330b794aa2fd3d5c6b9578cd70549002
Nkoyama <n.koyama24@gmail.com>
2019-05-06 17:39:10 +0900

    first

---
+++ GokonMaster/GokonMaster/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GokonMaster
+//
+//  Created by Nozomi Koyama on 2019/05/01.
+//  Copyright © 2019 Nozomi Koyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var gokon: UILabel!
+    @IBOutlet weak var master: UILabel!
+    @IBOutlet weak var gokonStartBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
