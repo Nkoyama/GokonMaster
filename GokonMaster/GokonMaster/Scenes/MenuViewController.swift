2840bfcbc7e3959b1f683a27c15dae5b65cc7c5e
NKoyama <n.koyama24@gmail.com>
2020-05-10 18:58:24 +0900

    メニュー画面着手

---
+++ GokonMaster/GokonMaster/Scenes/MenuViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MenuViewController.swift
+//  GokonMaster
+//
+//  Created by Nozomi Koyama on 2020/05/10.
+//  Copyright © 2020 Nozomi Koyama. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class MenuViewController: UIViewController {
+	
+}
