217b3dc5626be9d663e635d23119fe7b2c359cac
NKoyama <n.koyama24@gmail.com>
2020-05-17 17:11:32 +0900

    メニュー画面タイトル

--- GokonMaster/GokonMaster/Scenes/MenuViewController.swift
+++ GokonMaster/GokonMaster/Scenes/MenuViewController.swift
@@ -10,5 +10,28 @@ import UIKit
 import SnapKit
 
 class MenuViewController: UIViewController {
-	
+
+	// MARK: Views
+	let smallTitle		= UILabel()		// title
+	let changeSeatBtn	= UIButton()	// 席替えボタン
+	let exchangeContact	= UIButton()	// 連絡先交換ボタン
+
+
+	// MARK: Life Cycle
+	override func viewDidLoad() {
+		// background color
+		self.view.backgroundColor = UIColor.white
+
+		// title
+		self.smallTitle.numberOfLines = 2
+		self.smallTitle.text = "合コン\n    master"
+		self.smallTitle.textColor = UIColor.blue
+		self.smallTitle.font = UIFont.italicSystemFont(ofSize: 40.0)
+		self.view.addSubview(smallTitle)
+		self.smallTitle.snp.makeConstraints { (make) in
+			make.centerX.equalToSuperview()
+			make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).inset(70)
+		}
+
+	}
 }
