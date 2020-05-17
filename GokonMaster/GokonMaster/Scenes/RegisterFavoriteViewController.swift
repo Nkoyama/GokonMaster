990edc9c28b2f7c1a9363be10295385ace6c9532
NKoyama <n.koyama24@gmail.com>
2020-05-17 20:02:43 +0900

    順位ラベル

--- GokonMaster/GokonMaster/Scenes/RegisterFavoriteViewController.swift
+++ GokonMaster/GokonMaster/Scenes/RegisterFavoriteViewController.swift
@@ -15,6 +15,8 @@ class RegisterFavoriteViewController: UIViewController {
 	let backBtn				= UIButton()	// back button
 	let smallTitle			= UILabel()		// title
 	let secondTitle			= UILabel()		// secondary title
+	let dearName			= UILabel()
+	let message				= UILabel()
 
 
 	// MARK: Life Cycle
@@ -57,7 +59,58 @@ class RegisterFavoriteViewController: UIViewController {
 		self.view.addSubview(secondTitle)
 		self.secondTitle.snp.makeConstraints { (make) in
 			make.centerX.equalToSuperview()
-			make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).inset(200)
+			make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).inset(190)
+		}
+
+		// dear name
+		self.dearName.text = memberData[registeredNum].nickname + " さん"
+		self.dearName.textColor = UIColor.red
+		self.dearName.font = self.dearName.font.withSize(20)
+		self.view.addSubview(dearName)
+		self.dearName.snp.makeConstraints { (make) in
+			make.left.equalTo(self.view.safeAreaLayoutGuide.snp.left).inset(40)
+			make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).inset(240)
+		}
+		// message
+		self.message.text = "気に入っている方を登録してください。"
+		self.message.textColor = UIColor.black
+		self.view.addSubview(message)
+		self.message.snp.makeConstraints { (make) in
+			make.left.equalTo(self.view.safeAreaLayoutGuide.snp.left).inset(40)
+			make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).inset(270)
+		}
+
+		/* select favorite */
+		// 女の場合
+		if(memberData[registeredNum].sexIndex == 1) {
+			for i in 1...maleNum {
+				let lankLabel	= UILabel()		// 順位 label
+				lankLabel.text = String(i) + "位"
+				lankLabel.textColor = UIColor.black
+				lankLabel.textAlignment = NSTextAlignment.center
+				lankLabel.font = lankLabel.font.withSize(40.0 - 5.0*CGFloat(i))
+				self.view.addSubview(lankLabel)
+				lankLabel.snp.makeConstraints{ (make) in
+					make.left.equalTo(self.view.safeAreaLayoutGuide.snp.left).inset(50)
+					make.right.equalTo(self.view.safeAreaLayoutGuide.snp.left).inset(100)
+					make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).inset(250+70*i)
+				}
+			}
+		// 男の場合
+		} else {
+			for i in 1...femaleNum {
+				let lankLabel	= UILabel()		// 順位 label
+				lankLabel.text = String(i) + "位"
+				lankLabel.textColor = UIColor.black
+				lankLabel.textAlignment = NSTextAlignment.center
+				lankLabel.font = lankLabel.font.withSize(40.0 - 5.0*CGFloat(i))
+				self.view.addSubview(lankLabel)
+				lankLabel.snp.makeConstraints{ (make) in
+					make.left.equalTo(self.view.safeAreaLayoutGuide.snp.left).inset(50)
+					make.right.equalTo(self.view.safeAreaLayoutGuide.snp.left).inset(100)
+					make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).inset(270+70*i)
+				}
+			}
 		}
 	}
 
