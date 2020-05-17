f6b4298322456754cf229d879766bae0d97e6d9d
NKoyama <n.koyama24@gmail.com>
2020-05-17 17:27:36 +0900

    メニュー画面一旦完成

--- GokonMaster/GokonMaster/Scenes/MenuViewController.swift
+++ GokonMaster/GokonMaster/Scenes/MenuViewController.swift
@@ -12,9 +12,9 @@ import SnapKit
 class MenuViewController: UIViewController {
 
 	// MARK: Views
-	let smallTitle		= UILabel()		// title
-	let changeSeatBtn	= UIButton()	// 席替えボタン
-	let exchangeContact	= UIButton()	// 連絡先交換ボタン
+	let smallTitle			= UILabel()		// title
+	let changeSeatBtn		= UIButton()	// 席替えボタン
+	let exchangeContactBtn	= UIButton()	// 連絡先交換ボタン
 
 
 	// MARK: Life Cycle
@@ -33,5 +33,34 @@ class MenuViewController: UIViewController {
 			make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).inset(70)
 		}
 
+		// 席替えボタン
+		self.changeSeatBtn.setTitle("席替え", for: .normal)
+		self.changeSeatBtn.setTitleColor(UIColor.black, for: .normal)
+		self.changeSeatBtn.titleLabel?.font = UIFont.systemFont(ofSize: 25.0)
+		self.changeSeatBtn.backgroundColor = UIColor.init(red: 255/255, green: 187/255, blue: 0/255, alpha: 1)
+		self.changeSeatBtn.layer.borderColor = UIColor.gray.cgColor
+		self.changeSeatBtn.layer.borderWidth = 2.0
+		self.changeSeatBtn.layer.cornerRadius = 2.0
+		self.view.addSubview(changeSeatBtn)
+		self.changeSeatBtn.snp.makeConstraints{ (make) in
+			make.left.equalTo(self.view.safeAreaLayoutGuide.snp.left).inset(100)
+			make.right.equalTo(self.view.safeAreaLayoutGuide.snp.right).inset(100)
+			make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).inset(300)
+		}
+
+		// 連絡先交換ボタン
+		self.exchangeContactBtn.setTitle("連絡先交換", for: .normal)
+		self.exchangeContactBtn.setTitleColor(UIColor.black, for: .normal)
+		self.exchangeContactBtn.titleLabel?.font = UIFont.systemFont(ofSize: 25.0)
+		self.exchangeContactBtn.backgroundColor = UIColor.init(red: 255/255, green: 187/255, blue: 0/255, alpha: 1)
+		self.exchangeContactBtn.layer.borderColor = UIColor.gray.cgColor
+		self.exchangeContactBtn.layer.borderWidth = 2.0
+		self.exchangeContactBtn.layer.cornerRadius = 2.0
+		self.view.addSubview(exchangeContactBtn)
+		self.exchangeContactBtn.snp.makeConstraints{ (make) in
+			make.left.equalTo(self.view.safeAreaLayoutGuide.snp.left).inset(100)
+			make.right.equalTo(self.view.safeAreaLayoutGuide.snp.right).inset(100)
+			make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).inset(400)
+		}
 	}
 }
