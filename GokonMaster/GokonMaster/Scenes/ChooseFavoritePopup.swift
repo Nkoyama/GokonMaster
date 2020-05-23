8d05968ed74a1e66989ba71b2e5b3f32e5c98135
NKoyama <n.koyama24@gmail.com>
2020-05-23 13:18:07 +0900

    お気に入り選択ポップアップ画面仮作成

---
+++ GokonMaster/GokonMaster/Scenes/ChooseFavoritePopup.swift
@@ -0,0 +1,21 @@
+//
+//  ChooseFavoritePopup.swift
+//  GokonMaster
+//
+//  Created by Nozomi Koyama on 2020/05/23.
+//  Copyright © 2020 Nozomi Koyama. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class ChooseFavoritePopup: UIViewController {
+	// MARK: Views
+	
+	
+	// MARK: LifeCycle
+	override func viewDidLoad() {
+		// background color
+		self.view.backgroundColor = UIColor.init(red: 99/255, green: 78/255, blue: 134/255, alpha: 1)
+	}
+}
