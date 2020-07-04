9e1fa35da6cca654a4a3611a1926f73f4aa652d9
NKoyama <n.koyama24@gmail.com>
2020-07-04 15:38:52 +0900

    NGワードルール追加

--- GokonMaster/GokonMaster/Scenes/Games/NGWordsInitViewController.swift
+++ GokonMaster/GokonMaster/Scenes/Games/NGWordsInitViewController.swift
@@ -39,17 +39,26 @@ class NGWordsInitViewController: UIViewController {
 		self.view.addSubview(self.ruleLabel)
 		self.ruleLabel.snp.makeConstraints { (make) in
 			make.centerX.equalToSuperview()
-			make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).inset(200)
+			make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).inset(150)
 		}
 
 		// rule detail
-		self.ruleDetail.numberOfLines = 5
-		self.ruleDetail.text = "ルール"
+		self.ruleDetail.numberOfLines = 9
+		self.ruleDetail.text = "まず、全員に別々のNGワードが割り振られます。\n"
+							 + "NGワードはゲーム終了まで覚えておき、\n"
+							 + "誰にも教えないで下さい。\n"
+							 + "自分だけのNGワードを、誰かに言わせたら勝ち、\n"
+							 + "NGワードを言った人が負けとなります。\n"
+							 + "他の人のNGワードを言わないようにしながら、\n"
+							 + "自分のNGワードを言わせましょう。\n\n"
+							 + "最初に全員のNGワードを確認していきましょう！"
 		self.ruleDetail.textColor = UIColor.black
+		self.ruleDetail.adjustsFontSizeToFitWidth = true
 		self.view.addSubview(self.ruleDetail)
 		self.ruleDetail.snp.makeConstraints { (make) in
-			make.left.equalTo(self.view.safeAreaLayoutGuide.snp.top).inset(50)
-			make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).inset(250)
+			make.left.equalTo(self.view.safeAreaLayoutGuide.snp.left).inset(20)
+			make.right.equalTo(self.view.safeAreaLayoutGuide.snp.right).inset(20)
+			make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).inset(200)
 		}
 	}
 }
